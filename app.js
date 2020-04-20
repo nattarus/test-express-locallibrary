d2fbd8c6efeb9a4a77985c3e48cb107ebec1e0f8
nattarus <nattarus@gmail.com>
2020-04-20 14:31:30 +0700

    secend push

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ var app = express();
 //Set up mongoose connection
 app.use(helmet());
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://nattarus:022247041@cluster0-5dc2k.mongodb.net/local_library?retryWrites=true&w=majority';
+
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://nattarus:022247041@cluster0-5dc2k.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
